e9fc1926e472976a362174384ecf34e2a5000c30
cap <cap@ubuntu80>
2021-11-25 09:55:32 +1100

    client coded added.

--- client/src/app/employee.service.ts
+++ client/src/app/employee.service.ts
@@ -1,30 +1,29 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Employee } from "./employee";
-import { HttpClient } from "@angular/common/http";
-import { environment } from "src/environments/environment";
 
-@Injectable({
-    providedIn: 'root'
-})
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
+
+@Injectable({providedIn: 'root'})
 export class EmployeeService {
-    private apiServiceUrl = environment.apiBaseUrl;
+  private apiServerUrl = environment.apiBaseUrl;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient){}
 
-    public getEmployees(): Observable<Employee[]> {
-        return this.http.get<Employee[]>(`${this.apiServiceUrl}/employee/all`);
-    }
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
 
-    public addEmployee(employee: Employee): Observable<Employee> {
-        return this.http.post<Employee>(`${this.apiServiceUrl}/employee/add`, employee);
-    }
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
 
-    public updateEmployee(employee: Employee): Observable<Employee> {
-        return this.http.put<Employee>(`${this.apiServiceUrl}/employee/update`, employee);
-    }
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
 
-    public deleteEmployee(employeeId: number): Observable<void> {
-        return this.http.delete<void>(`${this.apiServiceUrl}/employee/delete/${employeeId}`);
-    }
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
 }
--- src/main/java/com/bitcloudies/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/bitcloudies/employeemanager/repo/EmployeeRepo.java
@@ -2,9 +2,11 @@ package com.bitcloudies.employeemanager.repo;
 
 import com.bitcloudies.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
     void deleteEmployeeById(Long id);
 
